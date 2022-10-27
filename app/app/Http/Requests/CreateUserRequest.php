7efdc2a67b807a70759b197fa30845c8913f9394
livek8888 <86755026+livek8888@users.noreply.github.com>
2022-10-27 16:22:33 +0900

    Feat: request model
    request 모델링

---
+++ app/app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * 아이디 규칙
+     * => 영문 필수, 숫자 허용, 특수기호(_ 만 허용)  최소 4글자  ~ 최대 12글자
+     *
+     * 비밀번호 규칙
+     * => 첫 문자는 대문자이며, 영문 + 숫자 + 특수기호 ( _,!,@,#,$,(,),%,^ 만 허용)  포함하여 최소 8자 ~ 최대 20자
+     */
+
+
+    public function rules()
+    {
+        return [
+            'account' => 'required|string',
+            'password' => 'required|string',
+            'name' => 'required|string',
+            'email' => 'required|string',
+        ];
+    }
+}
